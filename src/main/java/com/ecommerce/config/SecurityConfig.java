72312d0d9bdef9350ebd317bdfceb2c30e2fcbc0
Sudeep1837 <b122118@iiit-bh.ac.in>
2026-03-14 17:31:55 +0530

    Revert "Update SecurityConfig.java"
    This reverts commit 2b86f0191c776674471c8bd39ffcf8f44db925b6.

--- src/main/java/com/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/config/SecurityConfig.java
@@ -31,7 +31,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(AbstractHttpConfigurer::disable)
             .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable())) // Needed for H2 console
             .exceptionHandling(exception -> exception.authenticationEntryPoint(authEntryPoint))
@@ -65,33 +64,4 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-      @Bean
-    public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
-
-    org.springframework.web.cors.CorsConfiguration configuration =
-            new org.springframework.web.cors.CorsConfiguration();
-
-    configuration.setAllowedOrigins(
-            java.util.List.of(
-                    "http://localhost:8080",
-                    "http://localhost:8081",
-                    "http://localhost:5173"
-            )
-    );
-
-    configuration.setAllowedMethods(
-            java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
-    );
-
-    configuration.setAllowedHeaders(java.util.List.of("*"));
-
-    configuration.setAllowCredentials(true);
-
-    org.springframework.web.cors.UrlBasedCorsConfigurationSource source =
-            new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
-
-    source.registerCorsConfiguration("/**", configuration);
-
-    return source;
-}
 }
